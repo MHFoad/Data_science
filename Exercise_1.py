50dc393c70176bb233a4e95b72fae1d4d6d08fa1
Mohammad <mhfoad2@gmail.com>
2023-09-06 11:22:11 +0300

    First exercise

---
+++ Exercise_1.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello, world!")
+    # Enter your code here, this is the correct indentation
+
+# This makes sure the main function is not called immediatedly
+# when TMC imports this module
+if __name__ == "__main__":
+    main()

13d13dd3b1843d3d3f4d0cbfe99fa72bfb2f437e
Mohammad <mhfoad2@gmail.com>
2024-10-22 12:01:08 +0300

    new exercise

---
+++ Matrix_diagonal _manipulation_list_comprehensive_method.py
@@ -0,0 +1,20 @@
+def extract_and_transform_diagonals (matrix):
+    n=len(matrix) # size of the square matrix
+
+    main_daigonal= [2*(matrix[i][i]) if matrix[i][i]%2 ==0 else matrix[i][i] for i in range (n)] # in diagonal matrix row == column means i==j
+
+    anti_daigonal=[matrix[i][n-i-1]/2 if matrix[i][n-i-1] %2 !=0 else matrix[i][n-i-1] for i in range (n)] #anti diagonal elements of a square matrix can be found through matrix[i][n-i-1]
+
+    return (main_daigonal , anti_daigonal)
+
+
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+result = extract_and_transform_diagonals(matrix)
+print (result)

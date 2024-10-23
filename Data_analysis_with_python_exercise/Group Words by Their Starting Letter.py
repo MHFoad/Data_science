48e311a81b46ad5c11a9b7c2a6332ffe7a7b542b
Mohammad <mhfoad2@gmail.com>
2024-10-23 09:53:46 +0300

    modification1

--- Data_analysis_with_python_exercise/Group Words by Their Starting Letter.py
+++ Data_analysis_with_python_exercise/Group Words by Their Starting Letter.py
@@ -2,8 +2,12 @@ def group_of_words(given_list):
     word_dict={letter:{word:len(word) for word in given_list if word.startswith(letter)}
                for letter in sorted({word[0] for word in given_list})
                }
-    return word_dict
+    for key, value in word_dict.items():
+        print(key)
+        for key, value in value.items():
+            print(f"{key}: {value}")
+        print()
 words = ["apple", "apricot", "banana", "cherry", "date", "elderberry", "fig", "grape", "guava"]
 D=group_of_words(words)
-print (D)
+
 

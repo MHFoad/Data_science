05ee901c48a5f8fdf386b943c2d32bc5b9accc2d
Mohammad <mhfoad2@gmail.com>
2024-10-29 09:11:10 +0200

    part 2 5th Exercise part 1

---
+++ Data_analysis_with_python_exercise/Exercise_part_2/Exercise_2.5 _summary_Part_1.py
@@ -0,0 +1,45 @@
+import math
+import sys
+
+"""with open("./example.txt", 'r') as file:
+    result = [float(line.strip()) for line in file]
+    #return result
+
+# Calculate sum, average, and standard deviation if there are numbers
+# if numbers:
+total_sum = sum(result)
+average = total_sum / len(result)
+variance = sum((x - average) ** 2 for x in result) / (len(result) - 1)
+stddev = math.sqrt(variance)
+
+print("{:.6F}\t{:.6F}\t{:.6F}".format (total_sum, average, stddev))
+
+
+"""
+def summary(filename):
+    with open("./example.txt", 'r') as file:
+        result=[float(line.strip())for line in file]
+        #return result
+
+
+    # Calculate sum, average, and standard deviation if there are numbers
+    #if numbers:
+    total_sum = sum(result)
+    average = total_sum / len(result)
+    variance = sum((x - average) ** 2 for x in result) / (len(result) - 1)
+    stddev = math.sqrt(variance)
+
+    return total_sum, average, stddev
+
+
+
+def main():
+    
+    for filename in sys.argv[1:]:
+        total_sum, average, stddev = summary(filename)
+        print(f"File: {filename} Sum: {total_sum:.6f} Average: {average:.6f} Stddev: {stddev:.6f}")
+
+
+if __name__ == "__main__":
+    main()
+

d3cc59d498a08614b9eb44f91cb46780b30e3e0d
Mohammad <mhfoad2@gmail.com>
2024-10-29 09:26:27 +0200

    part 2 5th Exercise part 2

--- Data_analysis_with_python_exercise/Exercise_part_2/Exercise_2.5 _summary_Part_1.py
+++ Data_analysis_with_python_exercise/Exercise_part_2/Exercise_2.5 _summary_Part_1.py
@@ -18,18 +18,25 @@ print("{:.6F}\t{:.6F}\t{:.6F}".format (total_sum, average, stddev))
 """
 def summary(filename):
     with open("./example.txt", 'r') as file:
-        result=[float(line.strip())for line in file]
+        result=[]
+        for line in file:
+            try:
+                number=float(line.strip())
+                result.append(number)
+            except ValueError:
+                print(f"Warning: Skipping line due to ValueError: {line.strip()}")
+        #result=[float(line.strip())for line in file]
         #return result
 
 
     # Calculate sum, average, and standard deviation if there are numbers
-    #if numbers:
-    total_sum = sum(result)
-    average = total_sum / len(result)
-    variance = sum((x - average) ** 2 for x in result) / (len(result) - 1)
-    stddev = math.sqrt(variance)
+    if result:
+        total_sum = sum(result)
+        average = total_sum / len(result)
+        variance = sum((x - average) ** 2 for x in result) / (len(result) - 1)
+        stddev = math.sqrt(variance)
 
-    return total_sum, average, stddev
+        return total_sum, average, stddev
 
 
 
